5510a484719f1db809765eed1c586d82ada4c593
briyandyju09 <briyangd@gmail.com>
2024-07-28 20:04:36 +0400

    chore: Import necessary packages and set up API for PokeCardEX program

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-from datetime import datetime
-import time
-import os
-import tkinter as tk
-from tkinter import filedialog
+#Importing All packages
+import pandas as pd#data manipulation
+import requests#web scraping
+from bs4 import BeautifulSoup#web scraping
+from datetime import datetime#date and time
+import time#sleep
+import os#file management
+import tkinter as tk#gui
+from tkinter import filedialog#gui
 from tqdm import tqdm #progress bar
-import math
-import openpyxl
-import pyfiglet
+import math#math
+import openpyxl#excel
+import pyfiglet#ascii art
 from pyfiglet import Figlet
 import re
-from decimal import Decimal
-from pokemontcgsdk import Card
+from decimal import Decimal#decimal
+from pokemontcgsdk import Card#pokemontcgsdk
 from pokemontcgsdk import Set
 from pokemontcgsdk import Type
 from pokemontcgsdk import Supertype
@@ -21,11 +22,17 @@ from pokemontcgsdk import Subtype
 from pokemontcgsdk import Rarity
 from pokemontcgsdk import RestClient
 
+#Setting up the API
+
 RestClient.configure('f1624ad8-1cf7-4ea2-b55c-b01122c54aaa')
+
+#Defining the ASCII Art
 
 def ascii():
     result = pyfiglet.figlet_format("PokeCardEX") 
     print(result) 
+
+#Defining the input command
 
 def input_com():
     x = input("Enter a command: ")
@@ -51,5 +58,6 @@ def input_com():
         time.sleep(3)
         exit()
 
+#Running the functions
 ascii()
 input_com()
